08b0b5fd082cf073a150f09eb2ff7214f8097002
abstractigakis <ericstratigakis@gmail.com>
2022-12-01 01:18:43 -0500

    1

---
+++ 1/b.py
@@ -0,0 +1,10 @@
+with open("./data.txt") as f:
+    calsArr = []
+    currCals = 0
+    for line in f.readlines():
+        if line == '\n':
+            calsArr.append(currCals)
+            currCals=0
+        else:
+            currCals += int(line)
+    print(sum(sorted(calsArr)[-3:]))
