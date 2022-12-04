70ab6d28bb4a3837f50cfd97ae02ca4eb63583da
abstractigakis <ericstratigakis@gmail.com>
2022-12-03 23:56:45 -0500

    3

--- 3/b.py
+++ 3/b.py
@@ -10,7 +10,7 @@ with open("./data.txt") as f:
         comp1, comp2, comp3 = lines[g*3:g*3+3]
         comp1, comp2, comp3 = comp1[:-1], comp2[:-1], comp3[:-1]
 
-        numLetters =26*2
+        numLetters = 26*2
         counts1 = [0] * (numLetters)
         counts2 = [0] * (numLetters)
         counts3 = [0] * (numLetters)
